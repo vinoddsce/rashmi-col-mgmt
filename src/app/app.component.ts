d108a9bd5d047d2fe53819ee6a06036d9e4eb641
vinoddsce <vinod.dsce@gmail.com>
2020-01-24 20:07:17 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `<p>This is from Inline</p>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title: string = "College Management";
+}
