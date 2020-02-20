b42ae5719dc71abce44bb8ede44e08a7bc0c8c9f
vinoddsce <vinod.dsce@gmail.com>
2020-02-20 21:26:12 +0530

    ng template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+
+interface Item {
+  id: number,
+  name: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showMsg: boolean = true;
+  items: { id: number, name: string }[] = [{ id: 1, name: 'Item-1' }, { id: 2, name: 'Item-2' }, { id: 3, name: 'Item-3' }];
+
 }
